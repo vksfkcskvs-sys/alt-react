0bcb0765437baaec0226a3dc71a61cb6c0fe40a2
Kunal <vksfkcskvs@gmail.com>
2026-02-09 14:56:39 +0530

    made a simple card for exercising

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
+import {Bookmark} from 'lucide-react'
 
 const App = () => {
   return (
     <div className='parent'>
+
       <div className="card">
-        <div className="top">
-          <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-vector-illustration-market-136495269.jpg" alt="" />
-          <button>Save</button>
+        <div>
+          <div className="top">
+          <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" alt="" />
+          <button>Save <Bookmark size={12}  /></button>
         </div>
-        <div className="center"></div>
+        <div className="center">
+          <h3>Amazon <span>5 days ago</span></h3>
+          <h2>Senior UI/UX Designer</h2>
+          <div className="tag">
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        </div>
+        <div className="bottom">
+          <div>
+              <h3>$120/hr</h3>
+              <p>Mumbai, India</p>
+          </div>
+            <button>Apply Now</button>
+        </div>
       </div>
+
     </div>
   )
 }
