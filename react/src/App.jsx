44bdca8ef2303623589ecbc1882da50563f66972
Kunal <vksfkcskvs@gmail.com>
2026-02-09 13:58:54 +0530

    Initial commit

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className="card">
+        <div className="top">
+          <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-vector-illustration-market-136495269.jpg" alt="" />
+          <button>Save</button>
+        </div>
+        <div className="center"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
