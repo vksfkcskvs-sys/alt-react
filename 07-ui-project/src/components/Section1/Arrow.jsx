e6fe1545811f374fdbb0a95c99c6d36103ec65dd
Kunal <vksfkcskvs@gmail.com>
2026-02-10 15:07:06 +0530

    added a new card to the right section

--- 07-ui-project/src/components/Section1/Arrow.jsx
+++ 07-ui-project/src/components/Section1/Arrow.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ArrowUpRight } from 'lucide-react'
 
 const Arrow = () => {
   return (
--- 07-ui-project/src/components/Section1/RightContent.jsx
+++ 07-ui-project/src/components/Section1/RightContent.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import RightCard from './RightCard'
 
 const RightContent = () => {
   return (
-    <div className='h-full w-2/3 bg-blue-200'>
-      
+    <div className='h-full w-2/3 p-6 bg-blue-200'>
+      <RightCard />
     </div>
   )
 }
