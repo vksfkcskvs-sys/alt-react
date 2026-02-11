482d9cea58f9abb8b98b9294043cb23f7e2c2651
Kunal <vksfkcskvs@gmail.com>
2026-02-11 15:16:20 +0530

    started with the concept of form handling

--- 10-useState/src/App.jsx
+++ 10-useState/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [num, setNum] = useState([1, 2, 3, 4, 5])
+  const btnClicked = () => {
+     const newNum = [...num];
+     newNum.push(99);
+     console.log(newNum);
+     setNum(newNum);
+  }
+
+
   return (
-    <div>App</div>
+    <div>
+      <h1>{num}</h1>
+      <button onClick={btnClicked}>Click</button>
+    </div>
   )
 }
 
