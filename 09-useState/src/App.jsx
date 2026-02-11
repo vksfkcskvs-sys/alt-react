c09ee565a779c29993383ee2df1c04152bdace02
Kunal <vksfkcskvs@gmail.com>
2026-02-11 14:39:42 +0530

    cleared the concept of hooks

--- 09-useState/src/App.jsx
+++ 09-useState/src/App.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
+  const [num, setNum] = useState(0)
 
-  const [a, setA] = useState()
+  function increaseNum() {
+    setNum(num + 1)
+  }
+  function decreaseNum() {
+    setNum(num - 1)
+  }
+  function jumpBy7() {
+    setNum(num + 7)
+  }
 
   return (
     <div>
-      <h1>Value of a is {a}</h1>
-      <button>Click</button>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>Increase</button>
+      <button onClick={decreaseNum}>Decrease</button>
+      <button onClick={jumpBy7}>Jump by 7</button>
     </div>
   )
 }
