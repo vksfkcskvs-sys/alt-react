12d415445535482f464005966f8e11e014b89f7e
Kunal <vksfkcskvs@gmail.com>
2026-02-12 16:34:25 +0530

    got to know more about api calling and started with useEffect

---
+++ 15-apicalling/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [data, setData] = useState([])
+
+  const getData = async () => {
+    const response = await axios.get('https://picsum.photos/v2/list')
+    setData(response.data)
+  }
+
+  return (
+    <div>
+      <button onClick={getData}>Get Data</button>
+      <div>
+        {data.map(function(elem, idx) {
+          return <h3>Hello {idx}</h3>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
