b9d82fde99d61eb3eab6b2e80a519e524bedc020
Kunal <vksfkcskvs@gmail.com>
2026-02-16 16:27:28 +0530

    successfully created a new gallery app

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Card from './components/Card';
 
 const App = () => {
 
-  const getData = () => {
-    axios.get
+  const [userData, setUserData] = useState([]);
+
+  const [index, setIndex] = useState(1)
+
+  const getData = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+    setUserData(response.data)
   }
 
+  useEffect(function () {
+    getData()
+  }, [index])
+
+  let printUserData = <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>Loading...</h3>
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+      return <div key={idx}>
+        <Card elem={elem} />
+      </div>
+    })
+  }
 
   return (
-    <div className='bg-black h-screen p-4 text-white'>
-      <button
-        onClick={getData}
-        className='bg-green-500 active:scale-95 mb-3 px-5 py-2 rounded text-white'>
-        get data
-      </button>
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
+      <div className='flex h-[82%] flex-wrap gap-4'>
+        {printUserData}
+      </div>
+      <div className='flex justify-center gap-6 items-center p-4'>
+        <button
+        style={{opacity:index == 1?0.5:1}}
+          className='bg-amber-400 text-black rounded px-4 py-2 font-semibold text-sm cursor-pointer active:scale-95'
+          onClick={() => {
+            if(index > 1){
+              setIndex(index-1)
+              setUserData([])
+            }
+          }}
+        >
+          Prev
+        </button>
+        <h4>Page {index}</h4>
+        <button
+          className='bg-amber-400 text-black rounded px-4 py-2 font-semibold text-sm cursor-pointer active:scale-95'
+          onClick={() => {
+            setUserData([])
+            setIndex(index+1)
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   )
 }
